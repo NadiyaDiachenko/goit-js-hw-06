7230a3972e570a2a298db57a0f7bad03c6604943
NadiyaDiachenko <149389240+NadiyaDiachenko@users.noreply.github.com>
2024-03-05 10:24:12 +0200

    homework

--- js/task-3.js
+++ js/task-3.js
@@ -1,14 +1,46 @@
-function getElementWidth(content, padding, border) {
-    content = Number.parseFloat(content);
-    padding = Number.parseFloat(padding);
-    border = Number.parseFloat(border);
-    return content + padding * 2 + border * 2;
-    
-}
-getElementWidth("50px", "8px", "4px");
-getElementWidth("60px", "12px", "8.5px");
-getElementWidth("200px", "0px", "0px");
+// Напиши клас StringBuilder, який приймає один параметр initialValue — довільний рядок, який записується у приватну властивість value об'єкта, що створюється.
 
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+
+
+// Оголоси наступні методи класу:
+
+// getValue() — повертає поточне значення приватної властивості value.
+// padEnd(str) — отримує параметр str (рядок) і додає його в кінець значення приватної властивості value об'єкта, який викликає цей метод.
+// padStart(str) — отримує параметр str (рядок) і додає його на початок значення приватної властивості value об'єкта, який викликає цей метод.
+// padBoth(str) — отримує параметр str (рядок) і додає його на початок і в кінець значення приватної властивості value об'єкта, який викликає цей метод.
+
+class StringBuilder {
+    #value
+
+    constructor (initialValue){
+        this.#value = initialValue;
+    }
+
+    getValue(){
+        return this.#value
+    }
+
+    padEnd(str){
+        this.#value += str
+    }
+
+    padStart(str){
+        this.#value = str + this.#value;
+    }
+
+    padBoth(str) {
+        this.padStart(str)
+        this.padEnd(str)
+    }
+}
+
+
+
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
---
+++ training.js
@@ -0,0 +1,29 @@
+// class Soldier {
+//     constructor ({attack, health}) {
+//         this.attack = attack
+//         this.health = health
+//     }
+
+//     attack() {
+//        // ...
+//     }
+// }
+
+// class Warrior extends Soldier {
+//     constructor({attack, health, superSkill}) {
+//         super(attack, health);
+
+//         this.superSkill = superSkill
+//     }
+
+//     getSuperSkill() {
+//         return this.superSkill
+//     }
+// }
+
+// const soldier1 = new Soldier({attack: 10, health: 100})
+// const soldier2 = new Soldier({attack: 10, health: 102})
+
+// const warrior1 = new Warrior({attack: 10, health: 100, superSkill:'fire'})
+
+// console.log(soldier1, soldier2, warrior1);
